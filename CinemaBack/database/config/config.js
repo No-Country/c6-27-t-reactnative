98b9761bc06c0bf154bb9c93d50fc7acac908da2
diegonicita <diegonicita@gmail.com>
2022-08-18 14:26:51 -0300

    creating enviroment vars

--- CinemaBack/database/config/config.js
+++ CinemaBack/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    "username": process.env.USERDATABASE,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "mysql"
   }
 }
